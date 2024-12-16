3e4914c20bfdff5b9b7374144e4252a798b522ac
Michael Scholten <michael-scholten@hotmail.nl>
2024-12-16 18:56:18 +0100

    Segmentation fault protection through panic at run time instead of compile time.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+fn set_value(a: &mut [u64], value: u64) {
+    // Rust allows this at compile time, but will panic at runtime
+    a[3] = value;
+    println!("{}", a[3]);
+}
+
 fn main() {
     let mut a = [0];
 
-    // Rust denies this code at compile time.
-    a[3] = 0x7ffff7b36ceb;
-    println!("{}", a[3]);
+    set_value(&mut a, 0x7ffff7b36ceb);
 }
