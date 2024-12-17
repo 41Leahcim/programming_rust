88c2bdc41104302c6468341c2afb04a8e4bf17fe
Michael Scholten <michael-scholten@hotmail.nl>
2024-12-17 19:00:32 +0100

    Default initialized project

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-fn set_value(a: &mut [u64], value: u64) {
-    // Rust allows this at compile time, but will panic at runtime
-    a[3] = value;
-    println!("{}", a[3]);
-}
-
 fn main() {
-    let mut a = [0];
-
-    set_value(&mut a, 0x7ffff7b36ceb);
+    println!("Hello, world!");
 }
